3ff0aa3d15663d8152a312399b08e4daa06e610c
eerkaijun <kaijuneer@gmail.com>
2024-05-18 17:54:40 +0200

    set up server parameter

--- src/server.rs
+++ src/server.rs
@@ -1,14 +1,36 @@
-use crate::types::{PublicUserInfo, Transaction};
+use crate::types::Transaction;
 
 pub struct Server {
     // tuple of [H(x|tag), Transaction]
-    published_tags: Vec<(String, Transaction)>
+    published_tags: Vec<(String, Transaction)>,
+
+    /// private information retrieval parameters, for notation refer to 
+    /// https://forum.aztec.network/t/note-discovery-proposal-rfp/3247#hintlesspir-7
+    
+    // database dimension
+    m: u128,
+    // lwe dimension
+    n: u128,
+    // ciphertext modulus
+    q: u128,
+    // plaintext modules
+    p: u128,
+
+    // database of size m rows and m columns
+    matrix: Vec<Vec<u128>>,
 }
 
 impl Server {
     pub fn new() -> Self {
+        let matrix_size = 8;
+        let matrix = vec![vec![0; matrix_size]; matrix_size];
         Self {
-            published_tags: vec![]
+            published_tags: vec![],
+            m: matrix_size as u128,
+            n: 4,
+            q: 23,
+            p: 7,
+            matrix
         }
     }
 
