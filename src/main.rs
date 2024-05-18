84bff7dc01f8b614c45acdc2f64fa437a4c934d1
eerkaijun <kaijuneer@gmail.com>
2024-05-18 18:53:26 +0200

    wip encrypted query

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 pub mod protocol;
 pub mod server;
 pub mod types;
+pub mod utils;
 
 use crate::protocol::UtxoProtocol;
 use crate::server::Server;
+use crate::utils::{matrix_add, matrix_multiply, transpose};
 
 fn main() {
     println!("Starting UTXO note discovery protocol...");
@@ -12,5 +14,19 @@ fn main() {
     let server = Server::new();
 
     // Instantiate protocol
-    let protocol = UtxoProtocol::new(server);
+    // let protocol = UtxoProtocol::new(server);
+
+    let a_vector = server.a.clone();
+    let s_vector = vec![vec![1 as f64; 1]; server.n as usize];
+
+    let a_s = transpose(&matrix_multiply(&a_vector, &s_vector));
+    
+    let error_matrix = vec![0 as f64; server.m as usize];
+    let coefficient = server.q as f64 / server.p as f64;
+    let column_matrix = vec![coefficient,0.0,0.0,0.0,0.0,0.0,0.0,0.0];
+
+    let query = matrix_add(&matrix_add(&a_s, &error_matrix), &column_matrix);
+
+
+    server.process_query();
 }
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use crate::types::Transaction;
+use crate::utils::matrix_multiply;
 
 pub struct Server {
     // tuple of [H(x|tag), Transaction]
@@ -8,33 +9,66 @@ pub struct Server {
     /// https://forum.aztec.network/t/note-discovery-proposal-rfp/3247#hintlesspir-7
     
     // database dimension
-    m: u128,
+    pub m: u128,
     // lwe dimension
-    n: u128,
+    pub n: u128,
     // ciphertext modulus
-    q: u128,
+    pub q: u128,
     // plaintext modules
-    p: u128,
+    pub p: u128,
 
     // database of size m rows and m columns
-    matrix: Vec<Vec<u128>>,
+    db: Vec<Vec<f64>>,
+    // subset of database
+    pub a: Vec<Vec<f64>>,
+    // hint database
+    pub h: Vec<Vec<f64>>
 }
 
 impl Server {
     pub fn new() -> Self {
         let matrix_size = 8;
-        let matrix = vec![vec![0; matrix_size]; matrix_size];
+        let lwe_size = 4;
+        let mut matrix = vec![vec![0 as f64; matrix_size]; matrix_size];
+        let mut submatrix = vec![vec![0 as f64; lwe_size]; matrix_size];
+
+        // populate matrix with 0-63
+        let mut value = 0;
+        for i in 0..matrix_size {
+            for j in 0..matrix_size {
+                matrix[i][j] = value as f64;
+                value += 1;
+            }
+        }
+
+        // submatrix is a randomly sampled column subset of matrix
+        for i in 0..matrix_size {
+            for j in 0..lwe_size {
+                submatrix[i][j] = matrix[i][j];
+            }
+        }
+
+        // hint matrix is the matmul of matrix and submatrix
+        let hint = matrix_multiply(&matrix, &submatrix);
+
         Self {
             published_tags: vec![],
             m: matrix_size as u128,
-            n: 4,
+            n: lwe_size as u128,
             q: 23,
             p: 7,
-            matrix
+            db: matrix,
+            a: submatrix,
+            h: hint
         }
     }
 
-    pub fn process_query() {
+    pub fn process_query(self) {
         // Take in query argument and perform PIR, return encrypted data
+        println!("Database vector: {:?}", self.db);
+
+        println!("A vector: {:?}", self.a);
+
+        println!("H vector: {:?}", self.h);
     }
 }
